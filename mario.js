c73157cb2558c877cb6845d1c81765116ffe46cd
macronie <poornima08@yahoo.co.in>
2018-01-18 12:56:22 -0600

    pyramid done

--- mario.js
+++ mario.js
@@ -1,5 +1,6 @@
 
 printPyramid(5);
+printPyramid_left(5);
 
 
 /*
@@ -13,11 +14,34 @@ printPyramid(5);
  *       #####
  *      ######
  */
-function printPyramid(height) {
+// TODO
+    // print that pyramid! Put the code inside the fn
+function printPyramid(height){
+    console.log("The real assignment is pending...")
+}
+function printPyramid_left(height) {
     console.log("Uh oh... the pyramid is under construction.");
     console.log("Check back soon, our developers are hard at work as we speak!");
-
-    // TODO
-    // print that pyramid!
-
+    
+    for(var i=1; i<=height; i++){
+        var row ='';
+        for(var j=1; j<=(height -i); j++)
+        {
+            row +=' ';
+        }
+        for (var k=1; k<=i; k++){
+            if(i==1 && k==1)
+            {
+                row = ' '
+            }
+            else
+            {
+            row = row+'#';
+            }
+        }
+        console.log(row);
+    }
 }
+     
+
+
