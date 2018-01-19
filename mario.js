ebd9a4b6bcbad762704c1710c241e5dc7a7135f2
macronie <poornima08@yahoo.co.in>
2018-01-18 19:23:03 -0600

    modifications made for the mario2, pyramid is in the dom element

--- mario.js
+++ mario.js
@@ -11,7 +11,10 @@ function drawPyramid(height) {
 
     // TODO 2
     // delete the "under construction" element, ie the <div id="notice">
-
+    //(1) first look for parent id, find the child "id" which is to be modified, remove the child form the tree.
+    var para1 = document.getElementById("pyramid");
+    var child = document.getElementById("notice");
+    para1.removeChild(child);
 
     // for each row....
     for (var row = 0; row < height; row++) {
@@ -30,11 +33,20 @@ function drawPyramid(height) {
         }
 
         // you can delete this now
-        console.log(rowStr)
+        // console.log(rowStr)
+    
 
         // TODO 1
         // create an element whose inner text is rowStr,
         // and insert it as a child of the container <div id="pyramid">
+        //find the spot, to add the new element. create a new element, chreate a node for the new element,
+        //append the node as the child of the parent id
+        var para = document.createElement("p");
+        var node = document.createTextNode(rowStr);
+        para.appendChild(node);
 
+        var element = document.getElementById("pyramid");
+        element.appendChild(para);
     }
+    
 }
